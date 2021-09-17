f33a38bf833bec39cb5831206602a834bd70947f
reyadek <reyadek@gmail.com>
2021-09-17 17:55:03 +0700

    try reduce assets

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -79,9 +79,12 @@
 
         </div>
     </div>
-    <!-- Modal -->
+
+
+
+
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Leaderboard</h5>
@@ -111,13 +114,9 @@
                                         </td>
                                     </tr>
                                     @endforeach
-
                                 </tbody>
-
                             </table>
-
                         </div>
-
                     </div>
                 </div>
             </div>
