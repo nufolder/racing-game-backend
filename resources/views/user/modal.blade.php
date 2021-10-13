3b3675afe84cc790d55ee59f6e767978b917c26d
Dava  Eranda <davaeranda@Dava-Erandas-MacBook-Pro.local>
2021-10-13 15:04:57 +0700

    centered leaderboard

--- resources/views/user/modal.blade.php
+++ resources/views/user/modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content">
+        <div class="modal-content" style="margin-left: 5%;">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Leaderboard</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
