da204409b513c2975dc1b44394c322aa331db611
Dava  Eranda <davaeranda@Dava-Erandas-MacBook-Pro.local>
2021-10-14 10:16:46 +0700

    privacy policy done, terms conditions on progress

---
+++ resources/views/user/privacy-policy.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Privacy & Policy</title>
+</head>
+<body>
+    <p>
+        <ol>
+            <li>
+                Dengan mengakses generasijuara.com, Anda menyetujui Ketentuan Penggunaan ini. Jika Anda tidak setuju dengan Ketentuan Penggunaan ini, mohon untuk tidak mengakses generasijuara.com.
+            </li>
+            <li>
+                Kami berhak, atas kebijakannya sendiri, untuk memodifikasi, mengubah, menambah atau menghapus bagian dari Ketentuan Penggunaan ini kapan saja tanpa pemberitahuan. Mengakses generasijuara.com yang berkelanjutan akan menandakan bahwa Anda menerima adanya perubahan pada Ketentuan Penggunaan ini.
+            </li>
+            <li>
+                Dalam keadaan apa pun kami tidak akan bertanggung jawab kepada pihak mana pun atas kerugian langsung, tidak langsung, atau konsekuensi lainnya atas mengakses generasijuara.com.
+            </li>
+            <li>
+                Informasi pribadi yang disimpan dalam akan dihapus menggunakan langkah-langkah teknis. Penyimpanan informasi pribadi diperlukan untuk memenuhi persyaratan peraturan, mencegah penipuan , curang atau menyalahgunakan, atau menegakkan Kebijakan Privasi ini atau perjanjian lain yang mungkin kami miliki dengan pengguna.
+            </li>
+            <li>
+                Kami tidak memiliki atau mengontrol produk atau layanan pihak ketiga ini dan tidak bertanggung jawab atau bertanggung jawab atas keakuratan atau kontennya.
+            </li>
+            <li>
+                Jika Anda memerlukan informasi lebih lanjut atau memiliki pertanyaan tentang persyaratan penggunaan atau kebijakan privasi kami, jangan ragu untuk menghubungi kami melalui email di <a href="">Send email</a>
+            </li>
+        </ol>
+
+    </p>
+</body>
+</html>
