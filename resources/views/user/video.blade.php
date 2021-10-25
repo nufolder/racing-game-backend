2a759969ce36cf2d2fb76253a1e26f6f2582c61e
reyadek <reyadek@gmail.com>
2021-10-25 13:59:18 +0700

    update vide

--- resources/views/user/video.blade.php
+++ resources/views/user/video.blade.php
@@ -6,41 +6,43 @@
 
 @section('content')
 
-<div class="container pt-3 bg-white">
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    <div class="pb-2">
-                        <h4 class="text-center">Video</h4>
-                    </div>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Selamat !!</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p class="textfinishvideo"></p>
-                                </div>
-                                <div id="videoredirect" class="modal-footer">
-                                </div>
-                            </div>
-                        </div>
+<div class="container pt-3 bg-red">
+    <div class="col-md-12">
+        <div class="p-3">
+            <img src="{{ asset('images/logo-generasi-juara.png') }}" class="img-fluid">
+            <h1 class="text-center mt-3 text-white">
+                Video
+            </h1>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Selamat !!</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p class="textfinishvideo"></p>
+                    </div>
+                    <div id="videoredirect" class="modal-footer">
                     </div>
                 </div>
-
-                <div class="section d-flex justify-content-center embed-responsive embed-responsive-16by9 mb-4">
-                    <div id="player"></div>
-                </div>
-
             </div>
         </div>
+
+        <div class="section d-flex justify-content-center embed-responsive embed-responsive-16by9 mb-5">
+            <div id="player"></div>
+        </div>
+
+    </div>
+    <div class="pb-4">
+        <center><img src="{{ asset('images/logo-ahrt.png') }}" class="img-logo-bottom"></center>
     </div>
 </div>
+
+
 
 @endsection
