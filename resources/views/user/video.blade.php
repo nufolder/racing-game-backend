edea0b485d2482e0ca46fd3da8a57ea26c652d8d
reyadek <reyadek@gmail.com>
2021-10-28 11:45:40 +0700

    fix add heal and video

--- resources/views/user/video.blade.php
+++ resources/views/user/video.blade.php
@@ -6,8 +6,25 @@
 
 @section('content')
 
+<!-- Modal -->
+<div class="modal fade modal-video" id="myModal" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Selamat !!</h5>
+                <button type="button" class="ico-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="textfinishvideo"></p>
+            </div>
+            <div id="videoredirect" class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
 
-<div class="container pt-3 bg-red h-100">
+
+<div class="container pt-3 bg-red ">
     <div class="col-md-12">
         <div class="p-3">
             <img src="{{ asset('images/logo-generasi-juara.png') }}" class="img-fluid">
@@ -16,24 +33,7 @@
             </h1>
         </div>
 
-        <!-- Modal -->
-        <div class="modal fade modal-video" id="myModal" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Selamat !!</h5>
-                        <button type="button" class="ico-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p class="textfinishvideo"></p>
-                    </div>
-                    <div id="videoredirect" class="modal-footer">
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="section d-flex justify-content-center embed-responsive embed-responsive-16by9 mb-5">
+        <div class="d-flex justify-content-center embed-responsive embed-responsive-16by9 mb-5">
             <div id="player"></div>
         </div>
 
