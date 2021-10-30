ad81c28cf12800c51d197154b878b0ce7e7dce9f
Dava  Eranda <davaeranda@Dava-Erandas-MacBook-Pro.local>
2021-10-30 08:48:22 +0700

    modal nyawa habis, href fixed

--- resources/views/user/rider.blade.php
+++ resources/views/user/rider.blade.php
@@ -11,7 +11,7 @@
     <div class="row d-flex justify-content-center">
             @if (session('message'))
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                {{ session('message') }}
+                {!! session('message') !!}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
             @endif
