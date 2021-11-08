e7edb73cb592a76d577d1bc32f66a77ca1c75268
Dava  Eranda <davaeranda@Dava-Erandas-MacBook-Pro.local>
2021-11-08 15:35:01 +0700

    weekly winner update

--- resources/views/user/modal.blade.php
+++ resources/views/user/modal.blade.php
@@ -9,11 +9,15 @@
                     <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
                         <li class="nav-item" role="presentation">
                             <a class="nav-link active" id="all-tab" data-bs-toggle="tab" href="#all" role="tab"
-                                aria-controls="all" aria-selected="true">Global</a>
+                                aria-controls="all" aria-selected="true">Global Leader</a>
                         </li>
                         <li class="nav-item" role="presentation">
                             <a class="nav-link" id="weekly-tab" data-bs-toggle="tab" href="#weekly" role="tab"
                                 aria-controls="weekly" aria-selected="false">Weekly Leader</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="weekly-tab" data-bs-toggle="tab" href="#weekly-winner" role="tab"
+                                aria-controls="weekly" aria-selected="false">Weekly Winner</a>
                         </li>
                     </ul>
 
@@ -72,6 +76,36 @@
                                             </td>
                                             <td width="10%" class="text-left">
                                                 <b>{{ $value->score_weekly ?? '0'}}</b>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="weekly-winner" role="tabpanel" aria-labelledby="weekly-tab">
+                            <div class="mt-3 mb-3" style="text-align: center;">
+                                {{-- (tanggal - tanggal) --}}
+                            </div>
+                            <div class="table-responsive">
+                                <div id="showWWW">
+
+                                </div>
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="co" width="50" style="text-align: center;">#</th>
+                                            <th scope="col">Name</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        @foreach($week_win->slice(0, 4) as $key => $value)
+                                        <tr>
+                                            <td width="2%" class="text-center">{{ $key + 1 }}</td>
+                                            <td width="10%" class="text-left">
+                                                <b>{{ $value->user->name }}</b>
                                             </td>
                                         </tr>
                                         @endforeach
